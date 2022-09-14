59e9bbc2855e9aee4e11c31b2d875727413177ab
Ty Blackwell <tyeverettblackwell@gmail.com>
2022-09-14 14:52:27 -0400

    corrected queryselector ids

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-
+//Pulls  tab id from document to allow button creation on tab 
 const homeButton = document.querySelector("#home")
-const myCart = document.querySelector("#cart")
-const custService = document.querySelector("#customer")
-const aboutUs = document.querySelector("#bio")
+const myCart = document.querySelector("#mycart")
+const custService = document.querySelector("#customerservice")
+const aboutUs = document.querySelector("#aboutus")
 
-
+//Console.log id for tabs
 document.addEventListener("DOMContentLoaded", () =>{
     console.log(homeButton)
     console.log(myCart)
     console.log(custService)
     console.log(aboutUs)
 
+// Creates event listeners for tabs on click of tab
     homeButton.addEventListener("click", showHomePage)
     myCart.addEventListener("click", showCartPage)
     custService.addEventListener("click", showCustServicePage)
     aboutUs.addEventListener("click", showAboutUsPage)
 })
 
+// Alert for event listener on Home tab
 function showHomePage() {
     console.log("Home Page is clicked")
 }
 
+// Alert for event listener on Cart tab
 function showCartPage() {
     console.log("Cart should show")
 }
 
+// Alert for event listener on Customer Service tab
 function showCustServicePage(){
     console.log("LOL CUSTOMER SERVICE")
 }
 
+// Alert for event listener on About Us tab
 function showAboutUsPage(){
     console.log("Bio here")
 }
